024c629418ece172466f2b8b693744d04b22551f
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2023-02-11 23:18:18 -0600

    update

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
+#!/usr/bin/env python
+
+import asyncio
+import signal
 import os
-from websocket_server import WebsocketServer
 
-port = int(os.environ.get("PORT", 8000))
-
-
-def new_client(client, server):
-    print("New client connected and was given id %d" % client['id'])
-    server.send_message_to_all("Hey all, a new client has joined us")
+import websockets
 
 
-def message_received(client, server, message):
-    print("Client(%d) said: %s" % (client['id'], message))
-    server.send_message_to_all("Client(%d) said: %s" % (client['id'], message))
+async def echo(websocket):
+    async for message in websocket:
+        await websocket.send(message)
 
 
-server = WebsocketServer(port=port, host='0.0.0.0')
-server.set_fn_new_client(new_client)
-server.set_fn_message_received(message_received)
-server.run_forever()
+async def main():
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    async with websockets.serve(
+        echo,
+        host="",
+        port=int(os.environ["PORT"]),
+    ):
+        await stop
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
